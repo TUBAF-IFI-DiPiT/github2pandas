555ea956b8972655ae34889877cfc8088b696899
Maximilian Karl <maximilian.karl@hu-berlin.de>
2021-03-01 13:58:24 +0100

    Changed project structure

--- tests/test_VersionsAggregation.py
+++ tests/test_VersionsAggregation.py
@@ -7,7 +7,7 @@ import sys
 from pathlib import Path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.Versions_aggregation import cloneRepository,\
+from src.commits_edits.aggregation import cloneRepository,\
                                     generateDataBase,\
                                     generatePandasTables,\
                                     getCommitRawPandasTable,\
