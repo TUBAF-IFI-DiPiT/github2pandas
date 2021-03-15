ebcc052dfa632968558a8293a4a81cdf7a73b8ba
SebastianZug <Sebastian.Zug@informatik.tu-freiberg.de>
2021-03-15 19:12:20 +0100

    Apply new class structure on version aggregation submodule

--- github2pandas/version/aggregation.py
+++ github2pandas/version/aggregation.py
@@ -1,97 +1,109 @@
 import os
-from pathlib import Path
 import sqlite3
 import pickle
 import pandas as pd
 import pygit2 as git2
-import stat
 import git
-import shutil
 import git2net
+import shutil
+from pathlib import Path
 
-from .. import utility
+class AggVersion(object):
+   
+    VERSION_DIR = "Versions"
+    VERSION_REPOSITORY_DIR = "repo"
+    VERSION_COMMITS = "pdCommits.p"
+    VERSION_EDITS = "pdEdits.p"
+    VERSION_DB = "Versions.db"
+    NO_OF_PROCESSES = 1
 
-def clone_repository(git_repo_owner, git_repo_name, git_repo_dir,
-                    git_hub_token=None):
+    @staticmethod
+    def clone_repository(repo, data_root_dir, github_token=None):
 
-    if os.path.exists(git_repo_dir):
-        shutil.rmtree(git_repo_dir, onerror=readonly_handler)
-    callbacks = None
-    if git_hub_token:
-        callbacks = git2.RemoteCallbacks(
-            git2.UserPass(git_hub_token, 'x-oauth-basic'))
-    repo_ref = f"https://github.com/{git_repo_owner}/{git_repo_name}"
-    
-    git_repo_dir.mkdir(parents=True, exist_ok=True)
-    repo = git2.clone_repository(repo_ref, git_repo_dir, callbacks=callbacks)
+        git_repo_name = repo.name
+        git_repo_owner = repo.owner.login
+        
+        version_folder = Path(data_root_dir, AggVersion.VERSION_DIR)
+        version_folder.mkdir(parents=True, exist_ok=True)
 
-    existing_branches = list(repo.branches)
-    r = git.Repo.init(git_repo_dir)
+        repo_dir = version_folder.joinpath(AggVersion.VERSION_REPOSITORY_DIR)
+        if repo_dir.exists ():
+            shutil.rmtree(repo_dir.resolve())
 
-    for ref in repo.references:
-        branch_name = ref.split('/')[-1]
-        if branch_name != 'HEAD' and branch_name not in existing_branches:
-            print("  ", branch_name, sep=", ", end="")
-            try:
-                r.git.branch('--track', branch_name,
-                             'remotes/origin/'+branch_name)
-            except Exception:
-                print("An exception occurred")
-                print(" ")
+        callbacks = None
 
-    return True
+        if github_token:
+            callbacks = git2.RemoteCallbacks(
+                git2.UserPass(github_token, 'x-oauth-basic'))
+        repo_ref = f"https://github.com/{git_repo_owner}/{git_repo_name}"
+        repo = git2.clone_repository(repo_ref, repo_dir, callbacks=callbacks)
 
-# getting os permissions to remove (write) readonly files
-def readonly_handler(func, local_directory, execinfo):
+        existing_branches = list(repo.branches)
+        r = git.Repo.init(repo_dir)
 
-    os.chmod(local_directory, stat.S_IWRITE)
-    func(local_directory)
-
-
-def generate_data_base(git_repo_dir, data_dir, git_repo_name):
-
-    p = Path(data_dir)
-    p.mkdir(parents=True, exist_ok=True)
-    sqlite_db_file = Path(data_dir, git_repo_name + ".db")
-    if os.path.exists(sqlite_db_file):
-        os.remove(sqlite_db_file)
-    git2net.mine_git_repo(git_repo_dir, sqlite_db_file,
-                          no_of_processes=1,
-                          max_modifications=1000)
-
-    return True
+        for ref in repo.references:
+            branch_name = ref.split('/')[-1]
+            if branch_name != 'HEAD' and branch_name not in existing_branches:
+                print("  ", branch_name, end="")
+                try:
+                    r.git.branch('--track', branch_name,
+                                'remotes/origin/'+branch_name)
+                    print(" ")
+                except Exception:
+                    print(" -> An exception occurred")
 
 
-def generate_pandas_tables(data_dir, git_repo_name):
+        return True
 
-    sqlite_db_file = Path(data_dir, git_repo_name + ".db")
+    @staticmethod
+    def generate_data_base(data_root_dir, no_of_processes=1):
 
-    db = sqlite3.connect(sqlite_db_file)
-    pdCommits = pd.read_sql_query("SELECT * FROM commits", db)
-    pdEdits = pd.read_sql_query("SELECT * FROM edits", db)
+        version_folder = Path(data_root_dir, AggVersion.VERSION_DIR)
+        version_folder.mkdir(parents=True, exist_ok=True)
+        repo_dir = version_folder.joinpath(AggVersion.VERSION_REPOSITORY_DIR)
+        sqlite_db_file = version_folder.joinpath(AggVersion.VERSION_DB)
 
-    pd_commits_file = Path(data_dir, "pdCommits" + ".p")
-    with open(pd_commits_file, "wb") as f:
-        pickle.dump(pdCommits, f)
+        if os.path.exists(sqlite_db_file):
+            os.remove(sqlite_db_file)
+        git2net.mine_git_repo(repo_dir, sqlite_db_file,
+                            no_of_processes=AggVersion.NO_OF_PROCESSES,
+                            max_modifications=1000)
+        return True
 
-    pd_edits_file = Path(data_dir, "pdEdits" + ".p")
-    with open(pd_edits_file, "wb") as f:
-        pickle.dump(pdEdits, f)
+    @staticmethod
+    def generate_version_pandas_tables(data_root_dir):
 
-    return True
+        AggVersion.generate_data_base(data_root_dir, no_of_processes=1)
 
+        version_folder = Path(data_root_dir, AggVersion.VERSION_DIR)
+        sqlite_db_file = version_folder.joinpath(AggVersion.VERSION_DB)
 
-def get_commit_raw_pandas_table(data_dir):
-    pd_commits_file = Path(data_dir, "pdCommits" + ".p")
-    if pd_commits_file.is_file():
-        return pd.read_pickle(pd_commits_file)
-    else:
-        return pd.DataFrame()
+        db = sqlite3.connect(sqlite_db_file)
+        pdCommits = pd.read_sql_query("SELECT * FROM commits", db)
+        pdEdits = pd.read_sql_query("SELECT * FROM edits", db)
 
+        pd_commits_file = Path(version_folder, AggVersion.VERSION_COMMITS)
+        with open(pd_commits_file, "wb") as f:
+            pickle.dump(pdCommits, f)
 
-def get_edit_raw_pandas_table(data_dir):
-    pd_edits_file = Path(data_dir, "pdEdits" + ".p")
-    if pd_edits_file.is_file():
-        return pd.read_pickle(pd_edits_file)
-    else:
-        return pd.DataFrame()
+        pd_edits_file = Path(version_folder, AggVersion.VERSION_EDITS)
+        with open(pd_edits_file, "wb") as f:
+            pickle.dump(pdEdits, f)
+
+        return True
+
+    @staticmethod
+    def get_raw_commit(data_root_dir):
+        pd_commits_file = Path(data_root_dir, AggVersion.VERSION_DIR).joinpath(AggVersion.VERSION_COMMITS)
+        if pd_commits_file.is_file():
+            return pd.read_pickle(pd_commits_file)
+        else: 
+            return None
+
+    @staticmethod
+    def get_raw_edit(data_root_dir):
+        pd_edits_file = Path(data_root_dir, AggVersion.VERSION_DIR).joinpath(AggVersion.VERSION_EDITS)
+        if pd_edits_file.is_file():
+            return pd.read_pickle(pd_edits_file)
+        else: 
+            return None
