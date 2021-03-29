1b84596b408c538ef7d68873ca75dd6d867a6c14
Maximilian Karl <maximilian.karl@hu-berlin.de>
2021-03-29 10:29:31 +0200

    bugfix

--- github2pandas/workflows.py
+++ github2pandas/workflows.py
@@ -133,7 +133,7 @@ class Workflows(object):
         if check_for_updates:
             old_workflows = Workflows.get_workflows(data_root_dir)
             check_workflows = Utility.check_for_updates_paginated(workflows, old_workflows)
-            old_workflow_runs = Workflows.get_runs(data_root_dir)
+            old_workflow_runs = Workflows.get_workflows(data_root_dir,RUNS)
             check_workflow_runs = Utility.check_for_updates_paginated(workflow_runs, old_workflow_runs)
             if not check_workflows and not check_workflow_runs:
                 return
