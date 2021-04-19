08f73ed85bc162af28131261c82eeeb124c41aae
Maximilian Karl <maximilian.karl@hu-berlin.de>
2021-04-19 12:41:30 +0200

    rename folder

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('../github2pandas'))
 
 
@@ -34,6 +35,7 @@ extensions = ['sphinx.ext.todo',
              'sphinx.ext.viewcode',
              'sphinx.ext.autodoc',
              "m2r2",
+             'sphinx_rtd_theme',
              'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
@@ -50,12 +52,12 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_theme_path = ["_themes", ]
 
 
 source_suffix = [".rst", ".md"]
