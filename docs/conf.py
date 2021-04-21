b0f117751843a09d6760f11161d23dd089f84316
Maximilian Karl <maximilian.karl@hu-berlin.de>
2021-04-21 12:01:26 +0200

    ed

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../github2pandas'))
+sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 
 project = 'github2pandas'
