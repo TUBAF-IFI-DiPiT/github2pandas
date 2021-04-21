7b7a398edfd0d72878136a002712d128a2eabf35
Maximilian Karl <maximilian.karl@hu-berlin.de>
2021-04-21 11:32:03 +0200

    update docs

---
+++ docs/changes.rst
@@ -0,0 +1,7 @@
+Change log
+==========
+
+Version 1.54.1 (April 21, 2021)
+-----------------------------------
+
+* Publish project
--- docs/conf.py
+++ docs/conf.py
@@ -12,18 +12,18 @@
 #
 import os
 import sys
-import sphinx_rtd_theme
-sys.path.insert(0, os.path.abspath('../../github2pandas'))
-
+sys.path.insert(0, os.path.abspath('..'))
+from setup import version as setupVersion
 
 # -- Project information -----------------------------------------------------
 
 project = 'github2pandas'
-copyright = '2021, Maximilian Karl, Sebastian Zug'
-author = 'Maximilian Karl, Sebastian Zug'
+copyright = '2021, Maximilian Karl & Sebastian Zug'
+author = 'Maximilian Karl & Sebastian Zug'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.1'
+version = setupVersion
+release = setupVersion
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,21 +31,23 @@ release = '1.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo',
-             'sphinx.ext.viewcode',
-             'sphinx.ext.autodoc',
-             "m2r2",
-             'sphinx_rtd_theme',
-             'sphinx.ext.napoleon']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.mathjax"
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+# The suffix of source filenames.
+source_suffix = ".rst"
+# The master toctree document.
+master_doc = "index"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -57,7 +59,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_theme_path = ["_themes", ]
+html_static_path = ['_static']
 
-
-source_suffix = [".rst", ".md"]
+# Output file base name for HTML help builder.
+htmlhelp_basename = "github2oandasdoc"
