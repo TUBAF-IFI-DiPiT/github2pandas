70643519bb9dc176006086ec5d4a34d4bf763349
Maximilian Karl <maximilian.karl@hu-berlin.de>
2021-04-21 11:45:23 +0200

    edit

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../github2pandas'))
 from setup import version as setupVersion
 
 # -- Project information -----------------------------------------------------
