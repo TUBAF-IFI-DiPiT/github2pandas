13ea93494ce287c8c3183635f78ad9321e772a6d
Maximilian Karl <maximilian.karl@hu-berlin.de>
2021-04-21 11:46:46 +0200

    edit

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../github2pandas'))
 from setup import version as setupVersion
+sys.path.insert(0, os.path.abspath('../github2pandas'))
 
 # -- Project information -----------------------------------------------------
 
