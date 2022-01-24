ce9f181c1a409dd57b0adbc028e03e3297047b33
Maximilian Karl <maximilian.karl@hu-berlin.de>
2022-01-24 11:09:42 +0100

    add log_level

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -28,7 +28,7 @@ class TestRepositories(unittest.TestCase):
         github2pandas = GitHub2Pandas(self.github_token,self.data_root_dir, log_level=self.log_level)
         repo = github2pandas.get_repo(self.git_repo_owner, self.git_repo_name)
 
-        repository = Repository(github2pandas.github_connection, repo, self.data_root_dir)
+        repository = Repository(github2pandas.github_connection, repo, self.data_root_dir, log_level=self.log_level)
         repository.print_calls("Start repository")
         repository.generate_pandas_tables()
         repository.print_calls("End repository")
@@ -40,7 +40,7 @@ class TestRepositories(unittest.TestCase):
         github2pandas = GitHub2Pandas(self.github_token,self.data_root_dir, log_level=self.log_level)
         repo = github2pandas.get_repo("microsoft","vscode")
 
-        repository = Repository(github2pandas.github_connection, repo, self.data_root_dir)
+        repository = Repository(github2pandas.github_connection, repo, self.data_root_dir, log_level=self.log_level)
         repository.print_calls("Start large repository with companies")
         repository.generate_pandas_tables(contributor_companies_included=True)
         repository.print_calls("End large repository with companies")
