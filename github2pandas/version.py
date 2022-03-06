7891b6d62a888abc7fbabfa6f03736060a41fa9f
Sebastian Zug <Sebastian.Zug@informatik.tu-freiberg.de>
2022-03-06 09:16:18 +0100

    Delete timeout

--- github2pandas/version.py
+++ github2pandas/version.py
@@ -306,7 +306,6 @@ class Version(Core):
         import git2net
         git2net.mine_git_repo(self.repo_dir, self.sqlite_db_file,
                                 # extract_complexity=True,
-                                timeout=5,
                                 # extract_text=True,
                                 # no_of_processes=self.number_of_proceses,
                                 all_branches=True,
